ee9096396f8cd51b43b946a455d30fd3238c680d
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-09-17 01:23:02 +0530

    initialized express app

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const  express = require('express');
+
+
+
+const publicPath = path.join( __dirname,'../public');
+var app = express();
+const port = process.env.PORT||3000
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(port,()=>{
+	console.log(`Server is running on port ${port}`);
+});
+  
