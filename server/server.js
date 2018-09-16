6c35b36b0787a31c950e3a304883afed8aaee328
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-09-17 02:22:32 +0530

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const path = require('path');
 const  express = require('express');
-
-
+const http = require('http');
+const socketIO  = require('socket.io');
 
 const publicPath = path.join( __dirname,'../public');
 var app = express();
 const port = process.env.PORT||3000
 
 app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
+io.on('connection',()=>{
+	console.log('New User connected');
 
-app.listen(port,()=>{
+	socket.on('disconnect',()=>{
+		console.log('client disconnected');
+	})
+});
+
+
+server.listen(port,()=>{
 	console.log(`Server is running on port ${port}`);
 });
-  
+
+
+
